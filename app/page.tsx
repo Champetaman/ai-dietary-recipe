623b09f1ec9d82caa1dd387add7e62a1cfc2b107
Camilo Oviedo <caol29ster@gmail.com>
2024-07-31 20:07:58 +1000

    Ajustes de mejora en codigo y adicion validacion restricciones

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import { useChat } from "ai/react";
 import TimeSelector from "../components/TimeSelector";
 import FoodTypeSelector from "../components/FoodTypeSelector";
 import DietaryRestrictionsSelector from "../components/DietaryRestrictionsSelector";
-import ReligiousRestrictionsSelector from "../components/ReligiousRestrictionsSelector";
+import ReligiousDietsSelector from "../components/ReligiousDietsSelector";
 
 export default function Chat() {
   const { error, messages, isLoading, setInput, handleSubmit, reload, stop } =
@@ -17,69 +17,97 @@ export default function Chat() {
   const [selectedFoodType, setSelectedFoodType] = useState<string | null>(null);
   const [selectedDietaryRestrictions, setSelectedDietaryRestrictions] =
     useState<string[]>([]);
-  const [selectedReligiousRestrictions, setSelectedReligiousRestrictions] =
-    useState<string[]>([]);
+  const [selectedReligiousDiets, setselectedReligiousDiets] = useState<
+    string[]
+  >([]);
 
-  const onSubmit = () => {
+  const validateSelections = useCallback(() => {
     if (!selectedTime || !selectedFoodType) {
       alert(
         "Por favor selecciona al menos un tiempo estimado y la comida que te gustaría comer."
       );
-      return;
+      return false;
     }
+
+    if (
+      (selectedDietaryRestrictions.includes("Ninguna") &&
+        selectedDietaryRestrictions.length > 1) ||
+      (selectedReligiousDiets.includes("Ninguna") &&
+        selectedReligiousDiets.length > 1)
+    ) {
+      alert(
+        'No puedes seleccionar la opción: "Ninguna" con otras restricciones dietéticas.'
+      );
+      return false;
+    }
+    return true;
+  }, [
+    selectedTime,
+    selectedFoodType,
+    selectedDietaryRestrictions,
+    selectedReligiousDiets,
+  ]);
+
+  const onSubmit = useCallback(() => {
+    if (!validateSelections()) return;
 
     const prompt = `Sugiera una receta con los siguientes criterios:
-    1. **Tiempo:** ${selectedTime},
-    2. **Tipo de comida:** ${selectedFoodType},
-    ${
-      selectedDietaryRestrictions.length > 0
-        ? `3. **Restricción alimentaria:** ${selectedDietaryRestrictions.join(
-            ", "
-          )},`
-        : ""
-    }
-    ${
-      selectedReligiousRestrictions.length > 0
-        ? `4. **Restricción religiosa:** ${selectedReligiousRestrictions.join(
-            ", "
-          )},`
-        : ""
-    }
-    Por favor, proporcione una receta que se ajuste a los criterios mencionados anteriormente. Incluya lo siguiente en su respuesta:
-- **Nombre de la receta**
-- **Lista de ingredientes**
-- **Instrucciones paso a paso detalladas**
-- **Preparación y tiempo de cocción**
+      1. **Tiempo:** ${selectedTime},
+      2. **Tipo de comida:** ${selectedFoodType},
+      ${
+        selectedDietaryRestrictions.length > 0
+          ? `3. **Restricción alimentaria:** ${selectedDietaryRestrictions.join(
+              ", "
+            )},`
+          : ""
+      }
+      ${
+        selectedReligiousDiets.length > 0
+          ? `4. **Restricción religiosa:** ${selectedReligiousDiets.join(
+              ", "
+            )},`
+          : ""
+      }
+      Por favor, proporcione una receta que se ajuste a los criterios mencionados anteriormente. Incluya lo siguiente en su respuesta:
+      - **Nombre de la receta**
+      - **Lista de ingredientes**
+      - **Instrucciones paso a paso detalladas**
+      - **Preparación y tiempo de cocción**
 
-Asegurese de que la receta es clara, los ingredientes están en medición imperial y que sea fácil de seguir. Gracias!`;
+      Asegurese de que la receta es clara, los ingredientes están en medición imperial y que sea fácil de seguir. Gracias!`;
 
     setInput(prompt);
     handleSubmit(new Event("submit"));
-  };
+  }, [
+    setInput,
+    handleSubmit,
+    validateSelections,
+    selectedTime,
+    selectedFoodType,
+    selectedDietaryRestrictions,
+    selectedReligiousDiets,
+  ]);
 
-  const onReset = () => {
+  const onReset = useCallback(() => {
     setSelectedTime(null);
     setSelectedFoodType(null);
     setSelectedDietaryRestrictions([]);
-    setSelectedReligiousRestrictions([]);
+    setselectedReligiousDiets([]);
     setInput("");
     window.location.reload();
-  };
+  }, [setInput]);
 
   return (
     <div className="flex flex-col items-center w-full max-w-2xl py-24 mx-auto">
       <header className="mb-6 text-center">
         <h1 className="text-3xl font-bold">Recetario Inteligente</h1>
       </header>
-
       <TimeSelector onSelectTime={setSelectedTime} />
       <FoodTypeSelector onSelectFoodType={setSelectedFoodType} />
       <DietaryRestrictionsSelector
         onSelectRestriction={setSelectedDietaryRestrictions}
       />
-      <ReligiousRestrictionsSelector
-        onSelectRestriction={setSelectedReligiousRestrictions}
-      />
+      <ReligiousDietsSelector onSelectRestriction={setselectedReligiousDiets} />
 
       <div className="flex w-full space-x-2 mt-5">
         <button onClick={onSubmit} className="w-full btn">
@@ -119,16 +147,12 @@ Asegurese de que la receta es clara, los ingredientes están en medición imperi
       <div className="mt-4 p-4 bg-white rounded shadow w-full">
         {messages
           .filter((m) => m.role === "assistant")
-          .map((m) => {
-            return (
-              <div key={m.id} className="whitespace-pre-wrap mb-6">
-                <p className="text-lg font-medium mb-2">
-                  Platillo recomendado:
-                </p>
-                <p className="p-4 bg-gray-100 rounded-md">{m.content}</p>
-              </div>
-            );
-          })}
+          .map((m) => (
+            <div key={m.id} className="whitespace-pre-wrap mb-6">
+              <p className="text-lg font-medium mb-2">Platillo recomendado:</p>
+              <p className="p-4 bg-gray-100 rounded-md">{m.content}</p>
+            </div>
+          ))}
       </div>
 
       <footer className="mt-12 text-center text-gray-500">
