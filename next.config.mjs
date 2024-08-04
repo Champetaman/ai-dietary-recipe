c3ad53bb3cfe9d0213f15414482f6be2d91c5cb8
Camilo Oviedo <caol29ster@gmail.com>
2024-08-04 12:41:45 +1000

    Adicion dominio aceptado para imagenes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: [
+      // Add other domains if needed
+      "oaidalleapiprodscus.blob.core.windows.net",
+    ],
+  },
+};
 
 export default nextConfig;
